104b6377d9a9fb57f98c8ec796f14aafb0c49390
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-02 23:26:28 +0530

    Update page.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/total_order/page.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/total_order/page.tsx
@@ -1,11 +1,11 @@
 import { Footer } from "../_components/footer-section";
 import { QuickLink } from "../_components/bottun_link";
-import { DiscoverMain } from "./_components/total-order-main";
+import { ContactsMain } from "./_components/total-order-main";
 
 export default function DiscoverPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <DiscoverMain />
+      <ContactsMain />
       <QuickLink currentPage="discover" />
       <Footer currentPage="discover" />
     </main>
