fe0aa52fd364e1b5583c8b51a7ed561f86ebaf2c
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-02 19:22:50 +0530

    Update services-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/_components/services-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/_components/services-main.tsx
@@ -15,49 +15,11 @@ export function ServicesMain() {
       <div className="flex flex-col items-start font-light text-sm sm:text-base space-y-2">
  
         <div className="space-y-2 sm:space-y-6">
-          <div className="flex items-center">
-            <img
-              src="/online-shop.png"
-              alt="online shop"
-              className="h-8 sm:h-10 w-8 sm:w-10 mr-1 sm:mr-2"
-            />
-            <div className="font-bold">
-              <h3 className="text-base sm:text-lg">Business Discovery</h3>
-              <p className="text-[0.5rem] sm:text-xs leading-none sm:leading-4">
-                (discovering every business house)
-              </p>
-            </div>
-          </div>
-          <Separator className="bg-black" />
-          <div className="flex items-center">
-            <img
-              src="/fast-delivery.png"
-              alt="instant delivery bike"
-              className="h-8 sm:h-10 w-8 sm:w-10 mr-1 sm:mr-2"
-            />
-            <div className="font-bold">
-              <h3 className="text-base sm:text-lg">Instant Delivery Service</h3>
-              <p className="text-[0.5rem] sm:text-xs leading-none sm:leading-4">
-                (in Manipur)
-              </p>
-            </div>
-          </div>
-          <Separator className="bg-black" />
-          <div className="flex items-center">
-            <img
-              src="/fast-delivery-truck.png"
-              alt="fast delivery truck"
-              className="h-8 sm:h-10 w-8 sm:w-10 mr-1 sm:mr-2"
-            />
-            <div className="font-bold">
-              <h3 className="text-base sm:text-lg">
-                Commercial logistics service
-              </h3>
-              <p className="text-[0.5rem] sm:text-xs leading-none sm:leading-4">
-                (including outside states of Manipur)
-              </p>
-            </div>
-          </div>
+          
+        
+         
+         
+         
         </div>
       </div>
       <div className="sm:w-48">
