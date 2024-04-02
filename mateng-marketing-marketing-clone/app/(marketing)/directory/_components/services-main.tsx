c82c3ae81d3bbcd9b6f365a8266341ffaeaca20a
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-02 19:12:38 +0530

    Update directory-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/_components/services-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/_components/services-main.tsx
@@ -5,6 +5,7 @@ import { Separator } from "@/components/ui/separator";
 
 export function ServicesMain() {
   return (
+    
     <div
       className={cn(
         "pt-20 pb-10 sm:pt-32 flex flex-col sm:flex-row justify-center items-center space-y-8 sm:space-x-12 px-4 sm:px-0",
