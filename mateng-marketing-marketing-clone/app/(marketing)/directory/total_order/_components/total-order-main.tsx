714e8e36b50c732ec41555f45f754f4ae85819cc
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-02 23:24:59 +0530

    Update total-order-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/total_order/_components/total-order-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/total_order/_components/total-order-main.tsx
@@ -1,52 +1,49 @@
 import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { Mail, Phone } from "lucide-react";
 import { poppinsFont } from "../../_components/bottun_link";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { PhoneButton } from "../phone-button";
 
-export function DiscoverMain() {
-  const instagram = process.env.INSTAGRAM_PAGE || "#";
-  const facebook = process.env.FACEBOOK_PAGE || "#";
-  const youtube = process.env.YOUTUBE_CHANNEL || "#";
-  const linkedIn = process.env.LINKEDIN_PAGE || "#";
+export function ContactsMain() {
+  const phone = process.env.PHONE_NUMBER || "XXXXXXXXXX";
+  const whatsapp = process.env.WHATSAPP_NUMBER || "XXXXXXXXXX";
+  const email = process.env.CONTACT_EMAIL || "johndoe@example.com";
 
   return (
-    <div
-      className={cn(
-        "text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4",
-        poppinsFont.className
-      )}
-    >
-      <div className="font-medium text-xl sm:text-4xl w-[70%]">
-        We are currently building our discovery platform.
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      <div className="font-extrabold text-2xl sm:text-4xl">CONTACT US</div>
+      <div className="font-semibold text-base sm:text-lg w-[70%]">
+        For questions, technical assistance, or collaboration opportunities via
+        the contact information provided
       </div>
-      <div className="font-medium text-base sm:text-xl w-[70%] sm:pt-10">
-        stay with us to get updates
+      <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-10 pt-2 sm:pt-10">
+        <a
+          className=""
+          aria-label="Chat on WhatsApp"
+          href={`https://wa.me/91${whatsapp}`}
+        >
+          <img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.svg" />
+        </a>
+        <Button
+          className="rounded-xl w-52 font-semibold text-base h-12 sm:hidden"
+          asChild
+        >
+          <a
+            className={cn("", poppinsFont.className)}
+            href={`tel:${phone}`}
+            aria-label="contact"
+          >
+            <Phone className="h-6 w-6 inline mr-4" />
+            Contact
+          </a>
+        </Button>
+        <PhoneButton phone={phone}/>
       </div>
-      <div className="flex">
-        <Link href={facebook}>
-          <img
-            src={"/facebook.png"}
-            alt="facebook"
-            className="h-10 w-10 mr-4"
-          />
-        </Link>
-        <Link href={instagram}>
-          <img
-            src={"/instagram.png"}
-            alt="instagram"
-            className="h-10 w-10 mr-4"
-          />
-        </Link>
-        <Link href={linkedIn}>
-          <img
-            src={"/linkedin.png"}
-            alt="linkedin"
-            className="h-10 w-10 mr-4"
-          />
-        </Link>
-        <Link href={youtube}>
-          <img src={"/youtube.png"} alt="youtube" className="h-10 w-10 mr-4" />
-        </Link>
-      </div>
+      <a href={`mailto:${email}`}>
+        <Mail className="h-6 w-6 inline mr-2" />
+        {email}
+      </a>
     </div>
   );
 }
