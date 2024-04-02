1bb7f6a3fcbf02e90bdf8ca0e915a7eec1f78958
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-02 23:30:27 +0530

    Update page.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/page.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/page.tsx
@@ -1,12 +1,11 @@
 import { Footer } from "../_components/footer-section";
 import { QuickLink } from "../_components/bottun_link";
 
-import { DirectoryMain } from "./_components/services-main";
-
+import { Main } from "./_components/main-section";
 export default function DirectoryPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <DirectoryMain />
+      
       <QuickLink />
       <Footer currentPage="directory" />
     </div>
