7aee63f49e9fa44a53fb885d3c679462704d3e83
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 11:03:38 +0530

    Update directory-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/_components/directory-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/_components/directory-main.tsx
@@ -7,7 +7,7 @@ export function DirectoryMain() {
         "pt-20 pb-10 sm:pt-32 flex flex-col sm:flex-row justify-center items-center space-y-8 sm:space-x-12 px-4 sm:px-0"
       )}
     >
-           <h3>Business Discovery</h3>
+           
              
     </div>
   );
