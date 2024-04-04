a7e0965bed83b5098c15da10bd9a7867942d67b1
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 23:12:39 +0530

    Update page.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/padding_order/page.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/padding_order/page.tsx
@@ -3,7 +3,7 @@ import { Footer } from "../_components/footer-section";
 import { PaddingMain } from "./_components/padding-main";
 
 
-export default function AboutPage() {
+export default function PaddingPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <PaddingMain />
