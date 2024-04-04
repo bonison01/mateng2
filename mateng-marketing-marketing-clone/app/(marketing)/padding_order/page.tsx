86174a78972c18b08e4bd0e11b39f92db3e81da2
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 23:15:28 +0530

    Update page.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/padding_order/page.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/padding_order/page.tsx
@@ -1,15 +1,13 @@
 import { Footer } from "../_components/footer-section";
+import { QuickLink } from "../_components/quick-link-section";
+import { ContactsMain } from "./_components/contacts-main";
 
-import { PaddingMain } from "./_components/padding-main";
-
-
-export default function PaddingPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <PaddingMain />
-
-      
-      <Footer currentPage="padding" />
-    </main>
-  );
+export default function ContactsPage(){
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center">
+        <ContactsMain /> 
+        <QuickLink currentPage="contact-us"/>
+        <Footer />
+      </main>
+    )
 }
