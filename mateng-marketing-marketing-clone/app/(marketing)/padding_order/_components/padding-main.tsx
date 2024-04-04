8f09b47ff33ae57e0ed8e478efd0bf529fc151c5
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 23:08:31 +0530

    Add files via upload

---
+++ mateng-marketing-marketing-clone/app/(marketing)/padding_order/_components/padding-main.tsx
@@ -0,0 +1,13 @@
+
+import Link from "next/link";
+import { Content } from "./content";
+
+export function PaddingMain() {
+ 
+
+  return (
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      okokokoko
+    </div>
+  );
+}
