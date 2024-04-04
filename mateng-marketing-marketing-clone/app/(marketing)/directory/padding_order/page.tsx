3d6d5475fd1694e3c7893ade4d3e7a3ed0495ea1
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 08:48:34 +0530

    Update page.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/padding_order/page.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/padding_order/page.tsx
@@ -2,6 +2,7 @@ import { Footer } from "../_components/footer-section";
 import { Update } from "../_components/bottun_link";
 import { PaddingMain } from "./_components/padding-main";
 
+
 export default function DiscoverPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
