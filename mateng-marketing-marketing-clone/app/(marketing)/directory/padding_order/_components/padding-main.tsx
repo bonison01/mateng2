5eccb85043b8ff5d35e10c0318d4580563f6638f
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 08:58:43 +0530

    Update padding-main.tsx

--- mateng-marketing-marketing-clone/app/(marketing)/directory/padding_order/_components/padding-main.tsx
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/padding_order/_components/padding-main.tsx
@@ -1,23 +1,16 @@
 import { cn } from "@/lib/utils";
 
-import { poppinsFont } from "../../_components/bottun_link";
 
 export function PaddingMain() {
   
   return (
     <div
       className={cn(
-        "text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4",
-        poppinsFont.className
+        "text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4"
       )}
     >
-      <div className="font-medium text-xl sm:text-4xl w-[70%]">
-        We are currently building our discovery platform.
-      </div>
-      <div className="font-medium text-base sm:text-xl w-[70%] sm:pt-10">
-        stay with us to get updates
-      </div>
+     
       
-    <div/>
+    </div>
   );
 }
