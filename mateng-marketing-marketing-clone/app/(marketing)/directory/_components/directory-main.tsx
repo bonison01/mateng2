c72146976ed496a4deb970c5ff3cfadad7f0fb2b
bonison01 <164063307+bonison01@users.noreply.github.com>
2024-04-04 22:50:35 +0530

    Add files via upload

---
+++ mateng-marketing-marketing-clone/app/(marketing)/directory/_components/directory-main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import { Content } from "./content";
+
+export function DirectoryMain() {
+  return (
+    <div className="text-center pt-20 pb-10 sm:pt-32 flex flex-col justify-center items-center space-y-4">
+      <div className="border border-gray-400 rounded-lg p-4">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/padding_order">
+            <a>Test</a>
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+}
