b1173f0510704d842fbc3270bf84c094cd442689
bonison01 <bonisonkhumbongmayum@gmail.com>
2025-03-29 08:53:13 +0530

    dsf

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -154,11 +154,11 @@ function Navbar() {
                             <Tooltip>
                                 <TooltipTrigger>
                                     <div className="cartBtn relative rounded-full p-0.5 text-[0.95rem] leading-none hover:bg-[#222226a6]">
-                                        {cartItems.length > 0 && (
+                                        {/* {cartItems.length > 0 && (
                                             <div className="absolute -top-1 -right-1 bg-green-600 text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full shadow-lg">
                                                 {cartItems.length}
                                             </div>
-                                        )}
+                                        )} */}
                                         <div className="button-overlay"></div>
                                         <span className='rounded-full p-2.5 gap-3'>
                                             <svg className="cart" fill="white" viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
@@ -188,11 +188,11 @@ function Navbar() {
                                 <Tooltip>
                                     <TooltipTrigger>
                                         <div className="cartBtn relative rounded-full p-0.5 text-[0.95rem] leading-none hover:bg-[#222226a6]">
-                                            {cartItems.length > 0 && (
+                                            {/* {cartItems.length > 0 && (
                                                 <div className="absolute -top-1 -right-1 bg-green-600 text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full shadow-lg">
                                                     {cartItems.length}
                                                 </div>
-                                            )}
+                                            )} */}
                                             <div className="button-overlay"></div>
                                             <span className='rounded-full p-2.5 gap-3'>
                                                 <svg className="cart" fill="white" viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
